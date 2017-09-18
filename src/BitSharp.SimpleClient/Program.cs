31c4c234e22f6afc1faff8010e5575d25fea410d
Muhammad Azeez <Muhammad-Yasin@outlook.com>
2017-09-19 00:13:10 +0300

    Basic solution structure

---
+++ src/BitSharp.SimpleClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BitSharp.SimpleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
